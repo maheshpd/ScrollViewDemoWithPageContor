4024d0b8c6d59515d0830973848d4a52cb6f8f18
maheshprasad <mahesh223prasad@gmail.com>
2018-11-29 23:08:47 +0530

    Initial Commit

---
+++ ScrollViewDemoWithPageContor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewDemoWithPageContor
+//
+//  Created by Mahesh Prasad on 29/11/18.
+//  Copyright © 2018 CreatesApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
